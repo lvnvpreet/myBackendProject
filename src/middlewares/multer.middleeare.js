ce51b531a351e28772b8753c25b1ae7b31de8257
lvnvpreet <dhillonlovepreet147@gmail.com>
2024-11-29 00:08:31 +0530

    add multer-middleware and cloudnary file and use multer and cloudinary functionality

---
+++ src/middlewares/multer.middleeare.js
@@ -0,0 +1,17 @@
+import multer from "multers";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename:function (req, file, cb){
+        cb(null, file.originalname)
+    }
+})
+
+export const upload = multer({
+    storage,
+})
+
+
+// improve  this code at cb for error handling and at the filename 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: CLOUDINARY_API_KEY, 
+        api_secret: CLOUDINARY_API_SECRET
+    });
+    
+    
+const uploadOnCloudinary = async (localFilePath) =>{
+    try{ 
+        if(!localFilePath) return null
+        // Upload an image on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+               resource_type: 'auto'
+           })
+       // file has been uploaded successfull
+        console.log("file is uploaded on cloudinary",
+            response.url
+        );
+       return response;
+    } catch (error){
+        fs.unlinkSync(localFilePath) // remove the localy saved file as the upload operation got failed
+        return null;
+    }
+}
+
+
+export {uploadOnCloudinary}
