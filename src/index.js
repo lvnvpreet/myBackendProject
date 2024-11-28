69f4e88b207e7dfda18120aafd78d2d3706a9a8c
lvnvpreet <dhillonlovepreet147@gmail.com>
2024-11-28 18:33:11 +0530

    utils code discossion and app.js file update

--- src/index.js
+++ src/index.js
@@ -8,8 +8,21 @@ dotenv.config({
     path:'./env'
 })
 
-connectDB();
-
+connectDB()
+ 
+.then(() =>{
+    app.on("error", (error) => {
+        console.log("SERVER ERROR:", error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000 , () =>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+    
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
 
 
 
