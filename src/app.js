79ae977fe331abd1c424afc5efc22fc4c78deeb0
lvnvpreet <dhillonlovepreet147@gmail.com>
2024-11-29 02:09:00 +0530

    create controller and router

--- src/app.js
+++ src/app.js
@@ -11,11 +11,18 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({
-    extended:true,
-    limit:"16kb"
-}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())  
+
+//route import 
+import userRouter from './routes/user.route.js'
+
+
+//route declaration 
+app.use("/api/v1/users", userRouter) 
+
+// http://localhost:8000/api/v1/uers/register
+
 export { app }
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 // require('dotenv').config({path: './env'})
 
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
+    path: './env'
 })
 
 connectDB()
- 
-.then(() =>{
-    app.on("error", (error) => {
-        console.log("SERVER ERROR:", error)
-        throw error
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("SERVER ERROR:", error)
+            throw error
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`)
+        })
+
     })
-    app.listen(process.env.PORT || 8000 , () =>{
-        console.log(`Server is running at port : ${process.env.PORT}`)
+    .catch((err) => {
+        console.log("MONGO db connection failed !!!", err);
     })
-    
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!!", err);
-})
 
 
 
