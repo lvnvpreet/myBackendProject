2c4fb16a39d84aaea8d00b05385066095a1e0ea0
lvnvpreet <dhillonlovepreet147@gmail.com>
2024-12-02 12:54:17 +0530

    create importent models

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,4 +1,4 @@
-import moongose, { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const subscriptionSchema = new Schema({
     subscriber:{
@@ -11,4 +11,4 @@ const subscriptionSchema = new Schema({
     }
 },{timestamps:true})
 
-export const Subscription = moongose.model("Subscription", subscriptionSchema )
+export const Subscription = mongoose.model("Subscription", subscriptionSchema )
